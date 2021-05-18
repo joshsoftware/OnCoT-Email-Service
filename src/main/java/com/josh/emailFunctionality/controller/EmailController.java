c1a045076214d66fcc17f55a06b5c7b6f5059c26
Ajinkya <Ajinkyawaghdare97@gmail.com>
2021-05-18 16:25:33 +0530

    added cross origin

--- src/main/java/com/josh/emailFunctionality/controller/EmailController.java
+++ src/main/java/com/josh/emailFunctionality/controller/EmailController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ import com.josh.emailFunctionality.service.IEmailSendService;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin
 public class EmailController {
 
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
