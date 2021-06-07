068a023061b93378fd2986627b5c193bfe0879f4
Chinmay Patil <chinmaypatil265@gmail.com>
2021-06-07 10:33:00 +0530

    Implemented actuator

--- src/main/java/com/josh/emailFunctionality/entity/EmailEntity.java
+++ src/main/java/com/josh/emailFunctionality/entity/EmailEntity.java
@@ -1,24 +1,17 @@
 package com.josh.emailFunctionality.entity;
 
-<<<<<<< HEAD
-=======
 import java.util.Date;
 
->>>>>>> okay-branch-1
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-<<<<<<< HEAD
-
-=======
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
->>>>>>> okay-branch-1
 import com.josh.emailFunctionality.dto.EmailRequestDto;
 
 import lombok.AllArgsConstructor;
